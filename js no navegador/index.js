cf2e4f95dd9129cdb238c215cd04bf71219c6220
Pedro Cauan <90540372+pedrocauan@users.noreply.github.com>
2022-04-01 14:19:07 -0300

    Add files via upload

---
+++ js no navegador/index.js
@@ -0,0 +1,7 @@
+console.log(`Hello, Friend`);
+console.log(`Você está executando javascript no console do navegador.`);
+
+//No navegador podemos interagir com o usuario usando o alert.
+alert("Olá, amigo. Você caiu em uma janela de alerta. Clique em OK para que essa janela suma.");
+alert("Minha nossa.. Você acabou caindo em outra janela. Prometo que essa é a ultima.");
+
