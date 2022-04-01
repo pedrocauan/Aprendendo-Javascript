9ffd4839d254cada2b558f828eddf5c1bc81f63b
Pedro Cauan <90540372+pedrocauan@users.noreply.github.com>
2022-04-01 13:21:33 -0300

    Add files via upload

---
+++ console.log/aula01.js
@@ -0,0 +1,7 @@
+console.log("Olá, amigo!");
+console.log("'Olá amigo'");
+console.log('"Olá amigo"');
+console.log(`"'ÒLá amigo'"`);
+
+console.log(123456, 15.5, 'Olá amigo');
+console.log(2 + 'Olá amigo');
---
+++ console.log/ex001.js
@@ -0,0 +1,7 @@
+
+//     Exiba um console.log com a seguinte frase:
+// --> Meu nome é "Pedro", estou estudando JavaScript as 12 horas <--
+// - O numero 12 deve ser repreentado como um número e concatenado com a string "horas"
+
+// === RESPOSTA ===
+console.log(`Meu nome é "Pedro", estou  estudando JavaScript as`, 12, `horas`);
