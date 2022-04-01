c1c6d1c426c915983af136896e55610ec633f3c2
Pedro Cauan <90540372+pedrocauan@users.noreply.github.com>
2022-04-01 13:47:16 -0300

    Add files via upload

--- console.log/aula01.js
+++ console.log/aula01.js
@@ -1,7 +1,27 @@
-console.log("Olá, amigo!");
-console.log("'Olá amigo'");
-console.log('"Olá amigo"');
-console.log(`"'ÒLá amigo'"`);
+// ================ CONSOLE.LOG =================
+//  O objetivo da aula é  mostrar ao comando console.log
+//  que tem como função escrever algo na tela
+//  esta função utiliza parametros (instruções que
+//  ficam dentro os parenteses)
+//  
+//  Os parametros podem ser textos, números decimais, 
+//  numeros flutuantes, etc, etc..
+// ==============================================
 
-console.log(123456, 15.5, 'Olá amigo');
+console.log("Olá, amigo!");
+console.log("'Olá, amigo'");
+console.log('"Olá, amigo"');
+console.log(`"'Olá, amigo'"`);
+console.log(2500);
+console.log(3.14);
+
+/** Também é possível concatenar (juntar) duas instruções diferentes
+ *  com os operadores '+' e ','
+ */
+console.log(`\nOlá,` + ` ` + `amigo`);
 console.log(2 + 'Olá amigo');
+console.log(`Olá,`, `amigo`);
+console.log(123456, 15.5, 'Olá amigo\n');
+
+//Se utilizar o operador + com dois números, o resultado exibido será uma soma
+console.log(2 + 2);
