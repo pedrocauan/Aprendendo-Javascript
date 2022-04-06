037f92c0bd44a1284744d2b8fb726172d6b9575c
Pedro Cauan <90540372+pedrocauan@users.noreply.github.com>
2022-04-06 17:45:15 -0300

    Add files via upload

---
+++ balbla/index.js
@@ -0,0 +1,33 @@
+
+
+function eleGosta() {
+    window.document.body.innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/b7E0xhVXRTQ?&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    
+}
+
+
+function queIssoMeuFilhoCalma() {
+    window.document.body.innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/ou3BlCaj1DM?&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
+
+function Rapaz() {
+    window.document.body.innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/whns12cw4-w?&autoplay=1"  title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>`
+
+}
+
+function dancaGatinho() {
+    window.document.body.innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/OutIegT8HBg?&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
+}
+
+function Rapaz() {
+    window.document.body.innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/whns12cw4-w?&autoplay=1"  title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>`
+
+}
