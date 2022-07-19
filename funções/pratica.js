ec4b482e3a078ac727b9312420859f68e210615f
pedrocauan <mainyasuo92@gmail.com>
2022-07-18 23:18:58 -0300

    praticando IIFE

--- funções/pratica.js
+++ funções/pratica.js
@@ -1,61 +1,24 @@
+// funções auto invocadas | funções imediata || (IIFE)
 
+// IIFE -> Imediatle Invoked Function Expression -> ()()
 
-function rand(min = 1000, max = 3000){
-    const num = Math.random() * (max - min) +  min
-    return Math.floor(num)
-}
+//escopo:
+//(function(){})()
 
-function f1(callback) {
-    setTimeout(function () {
-        console.log("f1")
-        if(callback) callback()
-    }, rand())
+//Protege a função do escopo global 
+(function( idade, peso, altura){
+    
+    const sobrenome = "Silva"
+    function getNome(nome){
+        return nome + " " + sobrenome
+    }
+    
+    function falaNome(){
+        console.log(getNome("Pedro"))
+    }
 
+    falaNome()
+    console.log(idade, peso, altura)
+})(12,45.5, 1.82) //uma IIFE pode passar parametros  para as funções dentro dela 
 
-}
-
-function f2(callback) {
-    setTimeout(function () {
-        console.log("f2")
-        if(callback) callback()
-    }, rand())
-
-
-}
-
-function f3(callback) {
-    setTimeout(function () {
-        console.log("f3")
-        if(callback) callback()
-    }, rand())
-
-
-}
-
-//callback hell
-
-// f1(function(){
-//     f2(function(){
-//         f3(function(){
-//             console.log("Olá mundo")
-//         })
-//     })
-// })
-
-//As callbacks  servem para dar uma ordem sequencial a processos multiplos
-//f1 chama f2, f2 chamada f3 e f3 chama "hello mundo"
-f1(f1Callback())
-
-function f1Callback(){
-    f2(f2Callback)
-}
-
-function f2Callback() {
-    f3(f3Callback)
-}
-
-function f3Callback(){
-    console.log("Hello world")
-}
-
-
+const nome = "pedrao" //Impede que a variavel nome dentro da IIFE entre em conlito com essa daqui
