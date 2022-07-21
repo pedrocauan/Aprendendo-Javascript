ad14a6d153fd6c5af98411a3ce538e7f9cf783a9
pedrocauan <mainyasuo92@gmail.com>
2022-07-21 20:50:24 -0300

    splice

---
+++ array_avancado/splice/splice.js
@@ -0,0 +1,19 @@
+//               -5       -4      -3           -2      -1
+//                0        1       2            3       4
+const nomes = ["Maria", "Joao", "Eduardo", "Gabriel","Júlia"]
+
+//nomes.splice(indice atual, delete, elem1,elem2,elem3)
+
+/*pop*/
+//const removidos =  nomes.slice(-1, 1)
+
+/* shift */
+// const removidos = nomes.slice(0,1)
+
+/* push */
+//nomes.splice(nomes.length, 0, "Pedro", "trudy", "bob")
+
+/* unshift*/
+//nomes.splice(0, 0, "Pedro", "trudy", "bob")
+console.log(nomes)
+
