19b3250c63e7aa0b2a580e1574460dd923d8bbe6
pedrocauan <mainyasuo92@gmail.com>
2022-07-21 13:00:17 -0300

    revisando arrays

---
+++ array_avancado/revisao/index.js
@@ -0,0 +1,33 @@
+/* Cria array com construtor*/
+//const nomes = new Array("Pedro", "Joao", "Maria")
+
+// const novo = [...nomes] /* copia os dados de um array pra outro */
+
+//Arrays são valores por referencia. (referenciam um único endereço de memória)
+// const nomes = ["Eduardo", "Maria", "Joana", "Wallace", "Rosana"]
+// const novo = nomes.slice(2,-2)
+
+// console.log(novo)
+
+// const nome = "pedrones, silva, sousa"
+const nomes = [ 'pedrones', 'silva', 'sousa' ]
+const nome = nomes.join(" ")
+console.log(nome)
+
+
+
+
+
+
+
+
+// nomes.pop()/* remove o ultimo elemento*/
+// nomes.shift() /* remove o primeiro elemento*/
+// nomes.push("item") /*adiciona elemento no final*/
+// nomes.unshift("item") /*adiciona elemento no começo */
+// nomes.join(" ") /*une um array em uma string a partir de um separador*/
+//nome.split(" ") -> corta uma string a partir de um separador e transforma em array
+
+// == SHIFT E UNSHIFT DESLOCAM OS INDICES DO array ==
+
+
