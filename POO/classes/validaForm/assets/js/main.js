97743effcabf6d8cfd0be7ad72eeaafaeb0a1e3e
pedrocauan <mainyasuo92@gmail.com>
2022-08-27 00:54:36 -0300

    validando cpf

--- POO/classes/validaForm/assets/js/main.js
+++ POO/classes/validaForm/assets/js/main.js
@@ -19,10 +19,10 @@ class ValidaFormulario {
   }
 
   camposSaoValidos() {
-    let valid = true
+    let valid = true //verifica se o input é valido
 
     //remove todas as mensagens de erro quando o form é enviado
-    for(let errorText of  document.querySelectorAll(".error-text"))
+    for(let errorText of  this.formulario.querySelectorAll(".error-text"))
     {
       errorText.remove()
     }
@@ -31,14 +31,32 @@ class ValidaFormulario {
       //label com o nome dos inputs
       const label = campo.previousElementSibling.innerHTML
       
-      //STRING vazia = false, se a string tiver vazia ele da false e n entra na condição
+      // ========= VALIDA OS CAMPOS DO FORM ==========
+
+      //STRING vazia = false, se o input tiver vazio ele da false e n entra na condição
       if (!campo.value) {
-        this.criaErro(campo, `Campo ${label} invalido !!`)
+        this.criaErro(campo, `Campo ${label} vazio !!`)
         valid = false
+      }
+
+      //Ve se o CPF é valido
+      if(campo.classList.contains("cpf")){
+        if(!this.validaCPF(campo)){
+          valid = false
+        }
       }
 
 
     }
+  }
+  validaCPF(campo) {
+    const cpf = new ValidaCPF(campo.value)
+
+    if(!cpf.valida()) {
+      this.criaErro(campo, "CPF INVÁLIDO !!")
+      return false
+    }
+    return true
   }
 
   criaErro(campo, msg) {
@@ -48,7 +66,6 @@ class ValidaFormulario {
 
     //coloca o erro abaixo do form
     campo.insertAdjacentElement('afterend', div)
-
 
   }
 
