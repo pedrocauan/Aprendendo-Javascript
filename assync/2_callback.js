37e58262c62f63fdacd6208a3d3f41156d46ed6a
pedrocauan <mainyasuo92@gmail.com>
2022-08-28 15:44:04 -0300

    callback nas funções assincronas

---
+++ assync/2_callback.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Funções async executam  uma tarefa que precisa de um certo tempo para ser realizada
+ * sem que o programa pare sua execução
+ * ex:
+ *  -- ENVIAR UM EMAIL --
+ *   1 - checar
+     2 - hash (fazer criptografia)
+     . - upload //DEMORA UM TEMPO para ser feito
+     4 - salvar
+     . - enviar email   //DEMORA UM TEMPO PARA SER FEITO
+*/ 
+
+function enviarEmail(corpo, para, callBack) {
+    setTimeout(()=> {
+        console.log(`
+            Para: ${para}
+            -------------------------
+            ${`corpo`}
+            -------------------------
+            De: Pedrones
+            `)
+            callBack()
+    }, 8000)
+}
+console.log("Inicio do envio de e-mail!!") //executa
+enviarEmail("Oi, eu sou peedrones", "pedrones@gmail.com", () => {
+    console.log(`TUDO OK !!`)// executa depois  que o email chegar
+}) //executa mas nao para a execução do programa
+console.log(`email enviado !! Deve chegar em alguns minutos`) //executa
