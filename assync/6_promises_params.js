7445c39709c75d4605a487e459b8e630ba69fb27
pedrocauan <mainyasuo92@gmail.com>
2022-08-29 16:22:46 -0300

    promises aninhadas

--- assync/6_promises_params.js
+++ assync/6_promises_params.js
@@ -1,10 +1,25 @@
--
+
+function pegarId() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(5)
+        },1500)
+    })
+}
+
+function buscaEmailBanco(id) {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            resolve("pedrinho@gmail.com")
+        }, 2000)
+    } )
+}
+
 function enviarEmail(corpo, para) {
     //Quando trabalhamos com promises não temos necessidade de usar callback
     return new Promise((resolve, reject) => {
         setTimeout(() =>{
              let deuErro = false
-            console.log("Email enviado")
              if(!deuErro) {
                 resolve({time: 12, to: "pedrinho@gmail.com"}) //Promessa OK
 
@@ -14,12 +29,18 @@ function enviarEmail(corpo, para) {
              }
         }, 4000)
     })
-}                                             /*Pega  do resolve */
-enviarEmail("Olá", "Pedrinho@gmail.com").then( ({time, to})=> {
-    console.log(`
-        time: ${time} /*pega do parametro do then que pegou do resolve*/
-        to: ${to}
-    `)
-} ).catch((e) => {
-    console.log("parece que " + e )
-})
+}         
+console.log("INICIO")
+//executa primeira essa promise -> busca o ID do user no banco                            
+ pegarId().then(id => {
+    //depois essa -> busca o email no banco
+    buscaEmailBanco(id).then(email => {
+        //depois essa -> envia o email para o user com o id  pego
+        enviarEmail("Olá, como vai?", email).then(() => {
+            console.log("Email enviado para usuario com id: " + id)
+        }).catch( e => {
+            console.log(e)
+        })
+    })
+ })
+ console.log("FIM")
