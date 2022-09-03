0a1b1d4c8ae314329e9bb7978ee976b7deffdccb
pedrocauan <mainyasuo92@gmail.com>
2022-09-03 10:14:59 -0300

    babel e webpack

--- javascript-es6-tools/aula92/main.js
+++
@@ -1,8 +0,0 @@
-/*classe a ser transpilada com o babel*/
-class Pessoa {
-    constructor(nome,idade, sobrenome){
-        this.nome = nome
-        this.idade = idade
-        this.sobrneome = sobrenome
-    }
-}
