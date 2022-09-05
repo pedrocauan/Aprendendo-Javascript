d6a1a4fd7ee089f6b77661ba67ac792d860d14c1
pedrocauan <mainyasuo92@gmail.com>
2022-09-04 21:44:59 -0300

    express e definicao de crud

---
+++ node/aula05/express/aula06/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+
+const app = express()
+//         CRIAR   LER    ATT    APAGAR
+// CRUD -> Create, Read, Update,  Delete
+//         POST    GET    PUT    DELETE 
+
+// https://meusite.com/ <- GET -> Entregue a pagina  /
+// https://meusite.com/sobre <- GET -> entre a pagina /sobre
+// https://meusite.com/contato <- GET -> entregue a pagina /contato 
+
+app.get("/", (req,res) => {
+    res.send(`
+        <form action="/" method="POST">
+            nome: <input type="text" name="nome">
+            <input type="submit" value="enviar">
+        </form>
+    
+    `)
+})
+
+app.post("/", (req, res) => {
+    res.send(`recebi o formulário`)
+})
+
+app.get("/contato", (req,res) => {
+    res.send("Obrigado por estrar em contato com a gente")
+})
+
+app.listen(3000, () => {
+    console.log("https://localhost:3000")
+    console.log("rodando na 3000")
+})
+
+
