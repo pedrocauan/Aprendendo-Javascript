c6fd13804f3c4b1555a727289727441f7b854912
pedrocauan <mainyasuo92@gmail.com>
2022-09-10 13:03:28 -0300

    modelo webpack

---
+++ node/express/aula12-express-webpack/routes.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const route = express.Router()
+const homeController = require("./src/controllers/homeController.js")
+const contatoController = require("./src/controllers/contatoController.js")
+// ROTAS DA HOME
+route.get("/", homeController.paginaInicial)
+route.post("/", homeController.trataPost)
+
+//ROTAS CONTATO
+route.get("/contato", contatoController.paginaInicial)
+
+//
+
+
+module.exports = route
