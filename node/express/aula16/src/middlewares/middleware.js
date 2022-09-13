41766dca7e4e72d45566ddf8bdeb0dbfb449c4d4
pedrocauan <mainyasuo92@gmail.com>
2022-09-12 21:11:13 -0300

    renderizando no front

---
+++ node/express/aula16/src/middlewares/middleware.js
@@ -0,0 +1,14 @@
+exports.middlewareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = "Este é o valor da variavel local"
+    next()
+}
+
+exports.outroMiddleware = (req,res,next) => {
+
+    next()
+}
+
+// exports.outroMiddleware() = (req, res, next) => {
+
+//     next()
+// }
